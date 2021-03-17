f0f911d89a2cbc073062e012a950a0d22a924c39
fabinholima <fabio.delima21@gmail.com>
2021-03-16 22:40:43 -0400

    last finished changes

---
+++ init.el
@@ -0,0 +1,59 @@
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
+;;  Emacs Config Fábio Lima
+;;
+;;
+;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+
+
+
+(defvar bootstrap-version)
+(let ((bootstrap-file
+       (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
+      (bootstrap-version 5))
+  (unless (file-exists-p bootstrap-file)
+    (with-current-buffer
+        (url-retrieve-synchronously
+         "https://raw.githubusercontent.com/raxod502/straight.el/develop/install.el"
+         'silent 'inhibit-cookies)
+      (goto-char (point-max))
+      (eval-print-last-sexp)))
+  (load bootstrap-file nil 'nomessage))
+
+
+
+  (setq package-enable-at-startup nil)
+
+
+  (straight-use-package 'use-package)
+
+
+  (setq gc-cons-threshold (* 50 1000 1000))
+
+
+  (if (file-exists-p (expand-file-name "custom.el" user-emacs-directory))
+    (load-file (expand-file-name "custom.el" user-emacs-directory))
+  (org-babel-load-file (expand-file-name "custom.org" user-emacs-directory)))
+
+;; Make gc pauses faster by decreasing the threshold.
+(setq gc-cons-threshold (* 2 1000 1000))
+
+
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(org-bullets-bullet-list '("◉" "◎" "⚫" "○" "►" "◇"))
+ '(org-latex-compiler "xelatex")
+ '(pdf-misc-print-program-executable "/usr/bin/lpr"))
+   
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
